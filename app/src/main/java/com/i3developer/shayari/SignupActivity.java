d38259cb85c70f5009b27bf99a7ff8721a0cd88e
Saurabh <src9494@gmail.com>
2021-02-01 21:48:32 +0530

    Initial Commit

--- app/src/main/java/com/i3developer/shayari/SignupActivity.java
+++ app/src/main/java/com/i3developer/shayari/SignupActivity.java
@@ -1,34 +1,56 @@
 package com.i3developer.shayari;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ContextThemeWrapper;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignupActivity extends AppCompatActivity {
 
     private TextInputEditText phoneEdt,nameEdt;
     private Button submitBtn;
     private FirebaseAuth mAuth;
+    private String mVerificationId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         // To Display custom Action Bar
         setUpAppBar();
-
-        allInitializations();
+        allInitializations(); // All Initialization should be placed inside this method
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(TextUtils.isEmpty(nameEdt.getText())) {
+                    showToast(getApplicationContext(),"नाम आवश्यक है");
+                }
+                else if(TextUtils.isEmpty(phoneEdt.getText()) || phoneEdt.length() != 10) {
+                    showToast(getApplicationContext(),"मोबाइल नंबर आवश्यक है");
+                } else {
+                    sendVerificationCode("+91"+phoneEdt.getText());
+                }
             }
         });
     }
@@ -61,5 +83,57 @@ public class SignupActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
     }
 
+    private void sendVerificationCode(String phoneNumber) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+    private void verifyCode(String otp) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId,otp);
+        signInWithPhoneCredential(credential);
+    }
 
+    private void signInWithPhoneCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(SignupActivity.this, mAuth.getUid(), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(SignupActivity.this, "Failed to login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if(code != null) {
+                verifyCode(code);
+            }
+        }
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(SignupActivity.this, "Verification Failed", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            mVerificationId = s;
+        }
+    };
+    private void showToast(Context context, String text) {
+        ContextThemeWrapper themeWrapper = new ContextThemeWrapper(context,R.style.CustomAlertTheme);
+        Toast toast = Toast.makeText(themeWrapper,"",Toast.LENGTH_SHORT);
+        toast.setText(text);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
 }
