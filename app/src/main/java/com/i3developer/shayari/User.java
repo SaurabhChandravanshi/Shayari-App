bc29c1f50345399911dde201a4186e42565f7944
Sawan-Dewangan <sawandewangancph@gmail.com>
2021-02-02 18:59:46 +0530

    initial commit

---
+++ app/src/main/java/com/i3developer/shayari/User.java
@@ -0,0 +1,13 @@
+package com.i3developer.shayari;
+
+public class User {
+    public String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
